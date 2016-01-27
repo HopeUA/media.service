6ec878d44f17fdf2f0f2d929ea673bce3501d7c3
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-01-27 16:03:21 +0200

    Limit episode list

--- common/models/episode.js
+++ common/models/episode.js
@@ -1,3 +1,8 @@
 module.exports = function (Episode) {
-
+    Episode.observe('access', (ctx, next) => {
+        if (!ctx.query.limit || ctx.query.limit > 20) {
+            ctx.query.limit = 20;
+        }
+        next();
+    });
 };
