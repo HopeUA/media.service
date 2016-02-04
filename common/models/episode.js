aba3eb9ff19540a0b76f071aa7eedcd20d384516
Sergey Sadovoi <serg.sadovoi@gmail.com>
2016-02-04 17:52:33 +0200

    Now watching filter

--- common/models/episode.js
+++ common/models/episode.js
@@ -35,8 +35,12 @@ function linkNext(Model, episode) {
     }, { link: true });
 }
 
+function inject(episode) {
+
+}
 
 module.exports = function (Episode) {
+    // Inject max limit
     Episode.observe('access', (ctx, next) => {
         if (!ctx.query.limit || ctx.query.limit > 20) {
             ctx.query.limit = 20;
@@ -44,6 +48,7 @@ module.exports = function (Episode) {
         next();
     });
 
+    // Inject cover and links (prev, next)
     Episode.observe('loaded', (ctx, next) => {
         const episode = ctx.instance;
         const options = ctx.options || {};
@@ -68,4 +73,84 @@ module.exports = function (Episode) {
             next();
         });
     });
+
+    // New
+    Episode.remoteMethod('new', {
+        http: { verb: 'get' },
+        accepts: [
+            {arg: 'limit', type: 'Number', default: 10},
+            {arg: 'offset', type: 'Number', default: 0}
+        ],
+        returns: { arg: 'data', type: 'Array' }
+    });
+    Episode.new = (limit = 10, offset = 0, cb) => {
+        Episode.find({
+            order: 'publish DESC',
+            limit,
+            offset
+        }).then((result) => {
+            // Sort by publish DESC
+            result.sort((a, b) => {
+                if (a.publish < b.publish) {
+                    return 1;
+                }
+                if (a.publish > b.publish) {
+                    return -1;
+                }
+                return 0;
+            });
+
+            cb(null, result);
+        }).catch((error) => {
+            cb(error);
+        });
+    };
+
+    // Now
+    Episode.remoteMethod('now', {
+        http: { verb: 'get' },
+        accepts: [
+            {arg: 'limit', type: 'Number', default: 10}
+        ],
+        returns: { arg: 'data', type: 'Array' }
+    });
+    Episode.now = (limit = 10, cb) => {
+        (async () => {
+            const connector = Promise.promisifyAll(
+                Episode.getDataSource().connector
+            );
+            const db = await connector.connectAsync();
+            const collection = Promise.promisifyAll(
+                db.collection('Episode')
+            );
+            const rawData = await collection.aggregateAsync([
+                { $sample: { size: limit }}
+            ]);
+
+            let results = [];
+            for (const item of rawData) {
+                item.uid = item._id;
+                const episode = new Episode(item);
+                const context = {
+                    Model: Episode,
+                    instance: episode,
+                    isNewInstance: false,
+                    hookState: {},
+                    options: {}
+                };
+                try {
+                    await Episode.notifyObserversOf('loaded', context);
+                } catch (error) {
+                    console.error(error);
+                }
+                results.push(episode);
+            }
+
+            return results;
+        })().then((result) => {
+            cb(null, result);
+        }).catch((error) => {
+            cb(error);
+        });
+    };
 };
